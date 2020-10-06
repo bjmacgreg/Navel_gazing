783f661df068f85939451c142e5856294f18f112
Barbara MacGregor <1453-macgregb@users.noreply.umn.bootcampcontent.com>
2020-10-06 00:48:27 -0500

    dddd

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,7 @@ console.log(uniqueBBtypes);
 var uniqueWFreqs = [...new Set(data.metadata.map(item => item.wfreq))];
 console.log(uniqueWFreqs.sort(d3.ascending));
 
-//Ordered list of unique OTU IDs (length 1160, max value 3663)
+//Ordered list of unique OTU IDs
 let otuIDsUniqueBySample = [...new Set(data.samples.map(item => item.otu_ids))];
 let clump1 = d3.merge(otuIDsUniqueBySample);
 let uniq1 = [...new Set(clump1)];
@@ -27,8 +27,9 @@ console.log(uniqueOTUIDs);
 console.log(`The highest OTU ID is ` + d3.max (uniqueOTUIDs));
 console.log(`The lowest OTU ID is ` + d3.min (uniqueOTUIDs));
 console.log(`The number of unique OTU IDs is ` + uniqueOTUIDs.length);
+var extentBarCharty = d3.extent(uniqueOTUIDs);
 
-//Ordered list of unique sample values (length 246, max value 1305)
+//Ordered list of unique sample values
 let sampleValuesUniqueBySample = [...new Set(data.samples.map(item => item.sample_values))];
 let clump2 = d3.merge(sampleValuesUniqueBySample);
 let uniq2 = [...new Set(clump2)];
@@ -38,8 +39,9 @@ console.log(uniqueSampleValues);
 console.log(`The highest sample value is ` + d3.max (uniqueSampleValues));
 console.log(`The lowest sample value is ` + d3.min (uniqueSampleValues));
 console.log(`The number of different sample values is ` + uniqueSampleValues.length);
+var extentBarChartx = d3.extent(uniqueSampleValues);
 
-//Ordered list of unique OTU labels (length 245, max value 1305)
+//Ordered list of unique OTU labels
 let otuLabelsUniqueBySample = [...new Set(data.samples.map(item => item.otu_labels))];
 let clump3 = d3.merge(otuLabelsUniqueBySample);
 let uniq3 = [...new Set(clump3)];
@@ -47,6 +49,13 @@ var uniqueOTULabels = uniq3.sort(d3.ascending);
 //var maxSampleValue = d3.max (uniqueSampleValues);
 console.log(uniqueOTULabels);
 console.log(`The number of different OTU labels is ` + uniqueOTULabels.length);
+
+
+
+
+
+
+
 
 //ADD NAMES TO BUTTON
 d3.select("#selDataset")
@@ -58,21 +67,23 @@ d3.select("#selDataset")
   .attr("value", function (d) { return d; }); // corresponding value returned by the button
   
 var dropdownMenu = d3.selectAll("#selDataset").node();
-var tbody = d3.select("tbody");
-var thead = d3.select("thead");
+var metadata_tbody = d3.select("#metadata-table tbody");
+var metadata_thead = d3.select("#metadata-table thead");
+var overview_tbody = d3.select("#demoOverviewTable tbody");
+var overview_thead = d3.select("#demoOverviewTable thead");
 
-//FUNCTION TO FILL DEMOGRAPHIC DATA TABLE
+//FUNCTION TO FILL DEMOGRAPHIC DATA TABLE FOR SELECTED SUBJECT
 function fillTable(x) {
   //Clear existing data
     d3.select("#metadata-table").selectAll("tr").remove(); 
   //Put in new data 
     x.forEach((demoDatum) => {  
-      let labelrow = thead.append("tr");
+      let labelrow = metadata_thead.append("tr");
       Object.entries(demoDatum).forEach(([title, ]) => {
           let cell = labelrow.append("td");
           cell.text(title);
        });          
-      let datarow = tbody.append("tr");
+      let datarow = metadata_tbody.append("tr");
       Object.entries(demoDatum).forEach(([, value]) => {
           let cell = datarow.append("td");
           cell.text(value);
@@ -80,12 +91,14 @@ function fillTable(x) {
     });
   };
 
-//FUNCTION FOR BAR CHART OF TOP 10 OTUs
+//FUNCTION FOR BAR CHART OF TOP 10 OTUs FOR SELECTED SUBJECT
+//Made x and y axes constant for more meaningful comparisons between samples
 function barChart(currentOTUData) {
+  // Organize data
     var graphData = currentOTUData[0];
-    //console.log(graphData);
+    console.log(graphData);
     var graphDataArray = d3.zip(graphData.sample_values, graphData.otu_ids, graphData.otu_labels);
-    //console.log(graphDataArray);
+    
     // Select data for graph and choose graph type
     var trace1 = {
       x: graphDataArray.slice(0,10).map(row => row[0]),
@@ -95,7 +108,6 @@ function barChart(currentOTUData) {
       type: "bar",
       orientation: "h"
     };
-
     var chartData = [trace1];
 
   // Specify layout
@@ -105,28 +117,27 @@ function barChart(currentOTUData) {
         l: 100,
         r: 100,
         t: 100,
-        b: 100},
-      
+        b: 100},      
       xaxis: {title: "Sample value", titlefont: {
         family: 'Arial, sans-serif',
         size: 18,
         color: 'black'
-      }},
+      }, range: extentBarChartx},
       yaxis: {title: "OTU ID", titlefont: {
         family: 'Arial, sans-serif',
         size: 18,
         color: 'black'
-      }}
+      }, range: extentBarCharty}
     };
     Plotly.newPlot("bar", chartData, barlayout);
 };
 
-//FUNCTION FOR BUBBLE CHART OF TOP 10 OTUs
+//FUNCTION FOR BUBBLE CHART OF CURRENT OTUs FOR SELECTED SUBJECT
+//Made x axis constant for more meaningful comparisons between samples
 function bubbleChart(currentOTUData) {
     var graphData = currentOTUData[0];
-    // console.log(graphData);
     var graphDataArray = d3.zip(graphData.sample_values, graphData.otu_ids, graphData.otu_labels);
-    // console.log(graphDataArray);
+
     //Set bubble size and color
     var size = graphDataArray.map(row => row[0]);
     var color = graphDataArray.map(row => row[1]);
@@ -143,9 +154,9 @@ function bubbleChart(currentOTUData) {
         colorscale: 'viridis'
       }
     };
-
     var bubbledata = [trace1];
 
+  // Specify layout
     var bubblelayout = {
       title: 'Bubble Chart Hover Text',
       showlegend: false,
@@ -155,7 +166,7 @@ function bubbleChart(currentOTUData) {
         family: 'Arial, sans-serif',
         size: 18,
         color: 'black'
-      }},
+      }, range: extentBarCharty},
       yaxis: {title: "Sample value"}, titlefont: {
         family: 'Arial, sans-serif',
         size: 18,
@@ -166,7 +177,7 @@ function bubbleChart(currentOTUData) {
     Plotly.newPlot('bubble', bubbledata, bubblelayout);
 };
 
-//FUNCTION FOR WASH FREQUENCY GAUGE
+//FUNCTION FOR WASH FREQUENCY GAUGE FOR SELECTED SUBJECT
 function washGauge(currentDemoData) {
     washfreq = currentDemoData[0].wfreq;
     var washdata = [
@@ -187,29 +198,92 @@ function washGauge(currentDemoData) {
 };
 
 
-//FUNCTION TO UPDATE PAGE
+//FUNCTION TO UPDATE PAGE FOR SELECTED SUBJECT
 function updatePage() {
   //Get id of current subject from dropdown button
   var currentSubject = dropdownMenu.value;
-  // console.log(currentSubject);
 
   //Demographic and OTU data for current subject
   var currentDemoData = data.metadata.filter(function(d) { return d.id == currentSubject; });
-  //console.log(currentDemoData);
   var currentOTUData = data.samples.filter(function(d) { return d.id == currentSubject; });
-  // console.log(currentOTUData);
 
   //Make tables and charts
   fillTable(currentDemoData);
   barChart(currentOTUData);
   bubbleChart(currentOTUData);
   washGauge(currentDemoData);
-
 };  
+
+//FUNCTION TO FILL DEMOGRAPHIC OVERVIEW TABLE
+
+// Reference to button
+var filterbutton = d3.select("#filter-btn");
+
+function fillOverviewTable(x) {
+  //Clear existing data (needed if get multiple comparisons working...)
+  d3.select("#demoOverviewTable").selectAll("tr").remove();  
+  //Put in header rows
+    // x.forEach((subject) => {  
+      let labelrow = overview_thead.append("tr");
+      Object.entries(data.metadata[0]).forEach(([title, ]) => {
+          let cell = labelrow.append("td");
+          cell.text(title);
+       });          
+  //Add rows and fill them with data (assuming all data organized the same way)
+    x.forEach((subject) => {  
+      let row = overview_tbody.append("tr");
+      Object.entries(subject).forEach(([, value]) => {
+          let cell = row.append("td");
+          cell.text(value);
+        })});     
+      };
+//Display complete metadata dataset
+tableData = data.metadata;
+fillOverviewTable(data.metadata);
+console.log(tableData);
+//Filter button event handler
+filterbutton.on("click", function() {
+  //collect inputs
+  idInput = d3.select("#id-input").property ("value");
+  idInput =+ idInput;
+  ethnicityInput = d3.select("#ethnicity-input").property ("value"); 
+  genderInput = d3.select("#gender-input").property ("value"); 
+  ageInput = d3.select("#age-input").property ("value");
+  ageInput =+ ageInput;
+  locationInput = d3.select("#location-input").property ("value"); 
+  bellybuttonInput = d3.select("#bellybutton-input").property ("value"); 
+  washInput = d3.select("#wash-input").property ("value");
+  washInput =+ washInput;
+
+  //filter data
+  if (idInput) {selectedData1 = tableData.filter(tableData => (tableData.id === idInput))}
+  else {selectedData1 = tableData};
+  console.log(selectedData1);
+  if (ethnicityInput) {selectedData2 = selectedData1.filter(selectedData1 => (selectedData1.ethnicity === ethnicityInput))}
+  else {selectedData2 = selectedData1}; 
+  console.log(selectedData2);
+  if (genderInput) {selectedData3 = selectedData2.filter(selectedData2 => (selectedData2.gender === genderInput))}
+  else {selectedData3 = selectedData2};
+  console.log(selectedData3);
+  if (ageInput) {selectedData4 = selectedData3.filter(selectedData3 => (selectedData3.age === ageInput))}
+  else {selectedData4 = selectedData3};
+  console.log(selectedData4);
+  if (locationInput) {selectedData5 = selectedData4.filter(selectedData4 => (selectedData4.location === locationInput))}
+  else {selectedData5 = selectedData4};
+  console.log(selectedData5);
+  if (bellybuttonInput) {selectedData6 = selectedData5.filter(selectedData5 => (selectedData5.bellybutton === bellybuttonInput))}
+  else {selectedData6 = selectedData5};
+  console.log(selectedData6);
+  if (washInput) {selectedData7 = selectedData6.filter(selectedData6 => (selectedData6.wfreq === washInput))}
+  else {selectedData7 = selectedData6};
+  console.log(selectedData7);
+
+//display final selection in table
+  fillOverviewTable(selectedData7)});
+
+
+
 
 // EVENT HANDLER
 d3.selectAll("body").on("change", updatePage);
 });
-
-
-
